0a172b2e07518588b0fb705d2dc52f4fabbca737
Danya Bucenin <danyabucenin@gmail.com>
2026-03-13 20:27:04 +0300

    Deleted stb_image lib. Deleted Image class.

--- PhotoWizard/CppCore/core/core.hpp
+++ PhotoWizard/CppCore/core/core.hpp
@@ -4,42 +4,12 @@
 #include <vector>
 #include <iostream>
 #include <string>
+#include <cmath>
 
-#include "../lib/stb_image.h"
-#include "../lib/stb_image_write.h"
-
-struct Pixel
-{
-    uint8_t red;
-    uint8_t green;
-    uint8_t blue;
-};
-
-class Image
-{
-public:
-    Image() = default;
-
-    std::vector<Pixel>& getData()
-    {
-        return m_data;
-    }
-
-    bool load(const std::string& filename);
-    bool save(const std::string& filename) const;
-
-    int getWidth() const;
-    int getHeight() const;
-
-private:
-    std::vector<Pixel> m_data;
-    int m_width = 0, m_height = 0, m_channels = 0;
-
-};
 
 uint8_t clamp(double value);
 
-bool negativeFilter(Image& img);
-bool brightnessFilter(Image& img, double ratio);
-bool contrastFilter(Image& img, double ratio);
-bool exposureFilter(Image& img, double ratio);
+bool negativeFilter(std::vector<uint8_t>& pixel_array);
+bool brightnessFilter(std::vector<uint8_t>& pixel_array, double ratio);
+bool contrastFilter(std::vector<uint8_t>& pixel_array, double ratio);
+bool exposureFilter(std::vector<uint8_t>& pixel_array, double ratio);
